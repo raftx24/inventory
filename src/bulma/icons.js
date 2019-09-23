3a36fa0089c8e7c311e977aba75fa2b57f592dab
Mihai <mihai.ocneanu@gmail.com>
2019-09-23 17:08:42 +0300

    extracted menu icons

---
+++ src/bulma/icons.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBoxes, faTags, faWarehouse } from '@fortawesome/free-solid-svg-icons';
+import { faInventory } from '@fortawesome/pro-solid-svg-icons';
+
+library.add(faInventory, faBoxes, faTags, faWarehouse);
