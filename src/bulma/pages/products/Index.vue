136d81c5c8869fbaffcfa23fda5dbc591f01995e
Mihai <mihai.ocneanu@gmail.com>
2019-10-15 14:35:14 +0300

    updated label to title on filters

--- src/bulma/pages/products/Index.vue
+++ src/bulma/pages/products/Index.vue
@@ -5,7 +5,7 @@
                 <enso-date-filter class="box raises-on-hover"
                     v-model="params.dateInterval"
                     default="thirtyDays"
-                    :label="i18n('Updated')"
+                    :name="i18n('Updated')"
                     :interval="intervals.products.updated_at"/>
             </div>
         </div>
